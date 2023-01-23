40dc9f0f6b5cb256717540a679dad073582df8bc
mgalus <galusmarcin87@gmail.com>
2023-01-23 23:43:35 +0100

    1203811601310545

--- controllers/ProjectController.php
+++ controllers/ProjectController.php
@@ -34,7 +34,7 @@ class ProjectController extends \app\components\mgcms\MgCmsController
         $dataProvider = new ActiveDataProvider([
             'query' => Project::find()->where(['status' => [Project::STATUS_ACTIVE, Project::STATUS_PLANNED]]),
             'pagination' => [
-                'pageSize' => 3,
+                'pageSize' => 9,
             ],
             'sort' => [
                 'defaultOrder' => [
